d8fca622ff09b83ec1f871ac2ef62827fd73f6c3
Ruchiket Borse <RuchiketBorse1@gmail.com>
2026-07-08 14:58:26 +0530

    feat: add app:run:android scripts to automate full android compilation and installation loop

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -7,6 +7,11 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: "https",
   },
+  plugins: {
+    CapacitorCookies: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
